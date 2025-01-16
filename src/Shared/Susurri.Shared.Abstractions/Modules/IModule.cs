328661a0afd8812717750463ecb59e167b297abb
MagicCactus42 <magiccactus42@proton.me>
2025-01-16 16:58:06 +0100

    add module interfaces, exceptions base, and clock abstraction

---
+++ src/Shared/Susurri.Shared.Abstractions/Modules/IModule.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Susurri.Shared.Abstractions.Modules;
+
+public interface IModule
+{
+    string Name { get; }
+    void Register(IServiceCollection services);
+    void Initialize(IServiceProvider provider);
+}
