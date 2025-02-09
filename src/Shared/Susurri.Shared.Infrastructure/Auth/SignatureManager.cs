f34315d17d87c9f37053d7d9a603c329eb35f96a
MagicCactus42 <magiccactus42@proton.me>
2025-02-09 13:48:10 +0100

    implement module system, auth, and shared infrastructure

---
+++ src/Shared/Susurri.Shared.Infrastructure/Auth/SignatureManager.cs
@@ -0,0 +1,36 @@
+﻿using NSec.Cryptography;
+using Susurri.Shared.Abstractions.Auth;
+using Susurri.Shared.Infrastructure.Exceptions;
+
+namespace Susurri.Shared.Infrastructure.Auth;
+#nullable enable
+public sealed class SignatureManager : ISignatureManager
+{
+    private static readonly SignatureAlgorithm Algorithm = SignatureAlgorithm.Ed25519;
+
+    public bool IsValid(byte[]? signature, byte[]? publicKey, byte[]? data)
+    {
+        if (signature is null || publicKey is null || data is null)
+            return false;
+
+        try
+        {
+            var publicKeyObj = PublicKey.Import(Algorithm, publicKey, KeyBlobFormat.RawPublicKey);
+            return Algorithm.Verify(publicKeyObj, data, signature);
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    public byte[] Sign(byte[] data, Key privateKey)
+    {
+        if (data is null)
+            throw new InvalidSignatureData(nameof(data));
+        if (privateKey is null)
+            throw new InvalidSignatureData(nameof(privateKey));
+
+        return Algorithm.Sign(privateKey, data);
+    }
+}
---
+++ src/Shared/Susurri.Shared.Infrastructure/Modules/IModuleRegistry.cs
@@ -0,0 +1,20 @@
+﻿namespace Susurri.Shared.Infrastructure.Modules;
+
+public interface IModuleRegistry
+{
+    IEnumerable<ModuleBroadcastRegistration> GetModuleBroadcastRegistrations(string key);
+    void AddBroadcastAction(Type requestType, Func<object, Task> action);
+}
+
+public sealed class ModuleBroadcastRegistration
+{
+    public Type ReceiverType { get; }
+    public Func<object, Task> Action { get; }
+    public string Key => ReceiverType.Name;
+
+    public ModuleBroadcastRegistration(Type receiverType, Func<object, Task> action)
+    {
+        ReceiverType = receiverType;
+        Action = action;
+    }
+}
