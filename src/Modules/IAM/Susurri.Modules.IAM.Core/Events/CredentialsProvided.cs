d6c263e17d0fbf4eb661d73b11f4cab7f5b7185d
MagicCactus42 <magiccactus42@proton.me>
2025-02-27 17:20:31 +0100

    add IAM events, exceptions, and wire up extensions

---
+++ src/Modules/IAM/Susurri.Modules.IAM.Core/Events/CredentialsProvided.cs
@@ -0,0 +1,6 @@
+﻿using Susurri.Shared.Abstractions.Events;
+using Susurri.Shared.Abstractions.Messaging;
+
+namespace Susurri.Modules.IAM.Core.Events;
+
+public record CredentialsProvided(byte[] PublicKey, string Username) : IEvent;
---
+++ src/Modules/IAM/Susurri.Modules.IAM.Core/Exceptions/InvalidHexStringLengthException.cs
@@ -0,0 +1,10 @@
+﻿using Susurri.Shared.Abstractions.Exceptions;
+
+namespace Susurri.Modules.IAM.Core.Exceptions;
+
+public class InvalidHexStringLengthException : SusurriException
+{
+    public InvalidHexStringLengthException() : base($"Hex string length must be even.")
+    {
+    }
+}
