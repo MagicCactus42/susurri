2563d1998f7517eb1ef0390be19099078300ed2f
MagicCactus42 <magiccactus42@proton.me>
2025-04-15 13:56:53 +0200

    add User entity and value objects

---
+++ src/Modules/Users/Susurri.Modules.Users.Core/ValueObjects/UserId.cs
@@ -0,0 +1,21 @@
+﻿using Susurri.Modules.Users.Core.Exceptions;
+
+namespace Susurri.Modules.Users.Core.ValueObjects;
+
+public sealed record UserId
+{
+    public Guid Value { get; }
+
+    private UserId(){}
+    
+    public UserId(Guid value)
+    {
+        if (value == Guid.Empty)
+            throw new InvalidUserIdException(value);
+        
+        Value = value;
+    }
+
+    public static implicit operator Guid(UserId userId) => userId.Value;
+    public static implicit operator UserId(Guid userId) => new(userId);
+}
---
+++ src/Modules/Users/Susurri.Modules.Users.Core/ValueObjects/Username.cs
@@ -0,0 +1,22 @@
+﻿using Susurri.Modules.Users.Core.Exceptions;
+
+namespace Susurri.Modules.Users.Core.ValueObjects;
+
+public sealed record Username
+{
+    public string Value { get; }
+    
+    private Username(){}
+    
+    public Username(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value) || value.Length is > 30 or < 3)
+            throw new InvalidUsernameException(value);
+        
+        Value = value;
+    }
+    
+    public static implicit operator string(Username username) => username.Value;
+    public static implicit operator Username(string username) => new(username);
+    public override string ToString() => Value;
+}
