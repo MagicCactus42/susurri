64e26b46ab44fe578e50f928f5b7841fe975feb0
MagicCactus42 <magiccactus42@proton.me>
2025-05-07 17:05:05 +0200

    add users migrations, event handlers, and extensions

---
+++ src/Modules/Users/Susurri.Modules.Users.Core/Events/External/Handlers/SignedUpHandler.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Logging;
+using Susurri.Modules.Users.Core.Abstractions;
+using Susurri.Modules.Users.Core.Entities;
+using Susurri.Shared.Abstractions.Events;
+using Susurri.Shared.Abstractions.Time;
+
+namespace Susurri.Modules.Users.Core.Events.External.Handlers;
+
+public sealed class SignedUpHandler : IEventHandler<SignedUp>
+{
+    private readonly IUserRepository _userRepository;
+    private readonly ILogger<SignedUpHandler> _logger;
+    private readonly IClock _clock;
+
+    public SignedUpHandler(IUserRepository userRepository, ILogger<SignedUpHandler> logger, IClock clock)
+    {
+        _userRepository = userRepository;
+        _logger = logger;
+        _clock = clock;
+    }
+
+    public async Task HandleAsync(SignedUp @event)
+    {
+        var user = new User
+        {
+            UserId = Guid.NewGuid(),
+            Username = @event.Username,
+            PublicKey = @event.PublicKey,
+            CreatedAt = _clock.CurrentTime(),
+            LastSeenAt = _clock.CurrentTime()
+        };
+        
+        await _userRepository.AddAsync(user);
+        _logger.LogInformation($"User {user.Username} created with userId {user.UserId}");
+    }
+}
