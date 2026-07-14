0e505170f57deea6adb84856220eb736639ab1dc
MagicCactus42 <magiccactus42@proton.me>
2026-07-14 20:08:39 +0200

    gui: hook velopack app lifecycle so vpk pack accepts the binary

--- src/Bootstrapper/Susurri.GUI/Program.cs
+++ src/Bootstrapper/Susurri.GUI/Program.cs
@@ -1,14 +1,18 @@
 using Avalonia;
 using Avalonia.ReactiveUI;
 using System;
+using Velopack;
 
 namespace Susurri.GUI;
 
 sealed class Program
 {
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        VelopackApp.Build().Run();
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+    }
 
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
